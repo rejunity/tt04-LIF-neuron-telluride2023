97edc39db355cd1eb5afe19f6b549d7c41bc5703
rej <rzioma@acm.org>
2023-07-11 09:43:22 -0600

    Initial testbench connections

--- src/tb.v
+++ src/tb.v
@@ -28,18 +28,18 @@ module tb ();
     wire ena;
 
     tt_neuron tt_neuron_uut (
-    `ifdef GL_TEST
-        .vccd1( 1'b1),
-        .vssd1( 1'b0),
-    `endif
-    .ui_in      (ui_in),    // Dedicated inputs
-    .uo_out     (uo_out),   // Dedicated outputs
-    .uio_in     (uio_in),   // IOs: Input path
-    .uio_out    (uio_out),  // IOs: Output path
-    .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-    .ena        (ena),      // enable - goes high when design is selected
-    .clk        (clk),      // clock
-    .rst_n      (rst_n)     // not reset
+        `ifdef GL_TEST
+            .vccd1( 1'b1),
+            .vssd1( 1'b0),
+        `endif
+        .ui_in      (8'b1111_1111),    // Dedicated inputs
+        .uo_out     (uo_out),   // Dedicated outputs
+        .uio_in     (uio_in),   // IOs: Input path
+        .uio_out    (uio_out),  // IOs: Output path
+        .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+        .ena        (ena),      // enable - goes high when design is selected
+        .clk        (clk),      // clock
+        .rst_n      (rst_n)     // not reset
     );
 
 endmodule

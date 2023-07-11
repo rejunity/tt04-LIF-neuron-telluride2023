c74f36993422aa1555ec1d9248207d853b39fa25
rej <rzioma@acm.org>
2023-07-11 13:19:29 -0600

    Wire inside spike_generator

--- src/spike_generator.v
+++ src/spike_generator.v
@@ -3,7 +3,7 @@ module spike_generator #(parameter n_stage = 2) (
     input [(n_stage+1):0] minus_teta,
     output is_spike
 );
-    reg [(n_stage+2):0] s_out;
+    wire [(n_stage+2):0] s_out;
 
     // Calculate Adder_1 = u + minus_teta
     nbit_adder #(n_stage+2) Adder_1 (

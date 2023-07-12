f86eaf6fa9b9a72e6cba812d090b08f06be39544
rej <rzioma@acm.org>
2023-07-11 21:28:54 -0600

    Top module name must start with "tt_um_"

--- src/top.v
+++ src/top.v
@@ -1,5 +1,5 @@
 
-module tt_neuron (
+module tt_um_neuron (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path

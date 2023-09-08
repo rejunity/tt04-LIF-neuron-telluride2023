e55d92e0167a25a654eac21ea7fd0ff2376fafd3
rej <rzioma@acm.org>
2023-09-08 20:53:40 +0200

    Test neuron computations

--- src/test.py
+++ src/test.py
@@ -9,28 +9,79 @@ async def test_neuron(dut):
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
-    dut._log.info("reset")
+    dut._log.info("reset {shift=0, theta=5, membrane=0}")
     dut.rst_n.value = 0
-    dut.ui_in.value = 0b1101 # load weights
+    dut._log.info("load weights 1111_1101")
+    dut.ui_in.value = 0b1111_1101 # load weights
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut._log.info("accumulate")
-    dut.ui_in.value = 0b0001
+    dut._log.info("calculate")
+    dut._log.info("input 0000_0001")
+    dut.ui_in.value = 0b0000_0001
+    await ClockCycles(dut.clk, 1)
     for i in range(12):
         await ClockCycles(dut.clk, 1)
+        # print(dut.tt_um_rejunity_telluride2023_neuron_uut.w.value,
+        #         dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.w.value,
+        #         dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.x.value,
+        #         int(dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.u_out),
+        #         dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.is_spike.value,
+        #         dut.uo_out.value)
+        assert dut.uo_out[0] == [0,0,0,0, 1,0,0,0, 0,1,0,0][i]
 
-    dut.ui_in.value = 0b0011
+    dut.ui_in.value = 0b0000_0011
+    dut._log.info("input 0000_0011")
+    await ClockCycles(dut.clk, 1)
     for i in range(12):
         await ClockCycles(dut.clk, 1)
+        # print(dut.tt_um_rejunity_telluride2023_neuron_uut.w.value,
+        #         dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.w.value,
+        #         dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.x.value,
+        #         int(dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.u_out),
+        #         dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.is_spike.value,
+        #         dut.uo_out.value)
+        assert dut.uo_out[0] == [0,0,0,0, 0,0,0,0, 0,0,0,0][i]
 
-    dut.ui_in.value = 0b0111
+    dut.ui_in.value = 0b0000_0101
+    dut._log.info("input 0000_0101")
+    await ClockCycles(dut.clk, 1)
     for i in range(12):
         await ClockCycles(dut.clk, 1)
+        # print(dut.tt_um_rejunity_telluride2023_neuron_uut.w.value,
+        #         dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.w.value,
+        #         dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.x.value,
+        #         int(dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.u_out),
+        #         dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.is_spike.value,
+        #         dut.uo_out.value)
+        assert dut.uo_out[0] == [1,0,0,1, 0,1,0,0, 1,0,1,0][i]
 
-    dut.ui_in.value = 0b1111
+    dut.ui_in.value = 0b0000_0111
+    dut._log.info("input 0000_0111")
+    await ClockCycles(dut.clk, 1)
     for i in range(12):
         await ClockCycles(dut.clk, 1)
+        # print(dut.tt_um_rejunity_telluride2023_neuron_uut.w.value,
+        #         dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.w.value,
+        #         dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.x.value,
+        #         int(dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.u_out),
+        #         dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.is_spike.value,
+        #         dut.uo_out.value)
+        assert dut.uo_out[0] == [1,0,0,0, 0,1,0,0, 0,0,1,0][i]
+
+    dut.ui_in.value = 0b1000_0111
+    dut._log.info("input 1000_0111")
+    await ClockCycles(dut.clk, 1)
+    for i in range(12):
+        await ClockCycles(dut.clk, 1)
+        # print(dut.tt_um_rejunity_telluride2023_neuron_uut.w.value,
+        #         dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.w.value,
+        #         dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.x.value,
+        #         int(dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.u_out),
+        #         dut.tt_um_rejunity_telluride2023_neuron_uut.neuron_uut.is_spike.value,
+        #         dut.uo_out.value)
+        assert dut.uo_out[0] == [0,1,0,1, 0,0,1,0, 1,0,0,1][i]
+
 
     # figure out how enable this part only in `ifdef GL_TEST
     if False: # not GL_TEST:
